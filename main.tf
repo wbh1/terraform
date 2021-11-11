6c5cc9f263e9e1eeabf1fcceb6f87193125d3b1f
Will Hegedus <whegedus@linode.com>
2021-11-11 09:51:43 -0500

    Add a github runner

--- main.tf
+++ main.tf
@@ -11,6 +11,12 @@ provider "linode" {
   token = var.token
 }
 
+resource "linode_domain" "hegedus_wtf" {
+  type      = "master"
+  domain    = "hegedus.wtf"
+  soa_email = var.soa_email
+}
+
 resource "linode_instance" "grafana" {
   count           = 1
   image           = "linode/debian10"
@@ -23,16 +29,10 @@ resource "linode_instance" "grafana" {
   tags            = ["work"]
 }
 
-resource "linode_domain" "hegedus_wtf" {
-  type      = "master"
-  domain    = "hegedus.wtf"
-  soa_email = var.soa_email
-}
-
 resource "linode_domain_record" "grafana" {
   count       = length(linode_instance.grafana)
   domain_id   = linode_domain.hegedus_wtf.id
-  name        = linode_instance.grafana[count.index].label
+  name        = trimsuffix(linode_instance.grafana[count.index].label, ".${linode_domain.hegedus_wtf.domain}")
   target      = linode_instance.grafana[count.index].ip_address
   record_type = "A"
 }
@@ -41,4 +41,30 @@ resource "linode_rdns" "grafana" {
   count = length(linode_instance.grafana)
   address = linode_instance.grafana[count.index].ip_address
   rdns = linode_instance.grafana[count.index].label
+}
+
+resource "linode_instance" "githubrunner" {
+  count           = 1
+  image           = "linode/debian10"
+  label           = "githubrunner${count.index + 1}.hegedus.wtf"
+  group           = "githubrunner"
+  region          = var.region
+  type            = "g6-standard-1"
+  authorized_keys = [var.authorized_keys]
+  root_pass       = var.root_pass
+  tags            = ["work"]
+}
+
+resource "linode_domain_record" "githubrunner" {
+  count       = length(linode_instance.githubrunner)
+  domain_id   = linode_domain.hegedus_wtf.id
+  name        = trimsuffix(linode_instance.githubrunner[count.index].label, ".${linode_domain.hegedus_wtf.domain}")
+  target      = linode_instance.githubrunner[count.index].ip_address
+  record_type = "A"
+}
+
+resource "linode_rdns" "githubrunner" {
+  count = length(linode_instance.githubrunner)
+  address = linode_instance.githubrunner[count.index].ip_address
+  rdns = linode_instance.githubrunner[count.index].label
 }
