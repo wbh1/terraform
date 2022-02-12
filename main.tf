1022e9601f54dc8eb135945668d6421cd4f12b64
Will Hegedus <whegedus@linode.com>
2022-02-12 09:38:34 -0500

    feat: import ghost blog

--- main.tf
+++ main.tf
@@ -17,6 +17,51 @@ resource "linode_domain" "hegedus_wtf" {
   soa_email = var.soa_email
 }
 
+resource "linode_instance" "ghost" {
+  label             = "wbhegedus.me"
+  region            = var.region
+  tags = [
+    "personal",
+    "blog"
+  ]
+  type = "g6-nanode-1"
+
+  config {
+    kernel       = "linode/grub2"
+    label        = "My Debian 10 Disk Profile"
+    memory_limit = 0
+    root_device  = "/dev/sda"
+    run_level    = "default"
+    virt_mode    = "paravirt"
+
+    devices {
+      sda {
+        disk_label = "Debian 10 Disk"
+        volume_id  = 0
+      }
+
+      sdb {
+        disk_label = "512 MB Swap Image"
+        volume_id  = 0
+      }
+    }
+  }
+
+  disk {
+    filesystem = "ext4"
+    label      = "Debian 10 Disk"
+    read_only  = false
+    size       = 25088
+  }
+  disk {
+    filesystem = "swap"
+    label      = "512 MB Swap Image"
+    read_only  = false
+    size       = 512
+  }
+
+}
+
 resource "linode_instance" "k3s_server" {
   count           = 1
   image           = "linode/debian11"
@@ -38,9 +83,9 @@ resource "linode_domain_record" "k3s_server" {
 }
 
 resource "linode_rdns" "k3s_server" {
-  count = length(linode_instance.k3s_server)
+  count   = length(linode_instance.k3s_server)
   address = linode_instance.k3s_server[count.index].ip_address
-  rdns = linode_instance.k3s_server[count.index].label
+  rdns    = linode_instance.k3s_server[count.index].label
 }
 
 resource "linode_instance" "k3s_agent" {
@@ -64,7 +109,7 @@ resource "linode_domain_record" "k3s_agent" {
 }
 
 resource "linode_rdns" "k3s_agent" {
-  count = length(linode_instance.k3s_agent)
+  count   = length(linode_instance.k3s_agent)
   address = linode_instance.k3s_agent[count.index].ip_address
-  rdns = linode_instance.k3s_agent[count.index].label
+  rdns    = linode_instance.k3s_agent[count.index].label
 }
